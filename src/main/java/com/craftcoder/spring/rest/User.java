e1cf0fd403dcee4e7dad9a97405aee908239b1c2
Alexandre Gama <alexandre.gama.lima@gmail.com>
2017-04-23 21:05:40 -0300

    Cria Endpoint REST com Spring para retornar um usuario pelo seu id

--- src/main/java/com/craftcoder/spring/rest/User.java
+++ src/main/java/com/craftcoder/spring/rest/User.java
@@ -1,12 +1,18 @@
 package com.craftcoder.spring.rest;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@XmlRootElement(name = "user")
 public class User {
 
+	@XmlElement(name = "id")
 	private Long id;
 
 	@JsonProperty(value = "username")
+	@XmlElement(name = "username")
 	private String name;
 
 	User() {}
--- src/main/java/com/craftcoder/spring/rest/UserController.java
+++ src/main/java/com/craftcoder/spring/rest/UserController.java
@@ -2,22 +2,28 @@ package com.craftcoder.spring.rest;
 
 import java.util.Collection;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class UserController {
 
 	private UserDao dao = new UserDao();
 	
 	@RequestMapping(value = "/users", method = RequestMethod.GET)
-	@ResponseBody
 	public Collection<User> findAll() {
 		Collection<User> users = dao.findAll();
 		
 		return users;
+	}
+	
+	@RequestMapping(value = "/users/{userId}", method = RequestMethod.GET, produces = "application/xml")
+	public User findById(@PathVariable(name = "userId") Long id) {
+		User user = dao.findById(id);
+		
+		return user;
 	}
 	
 }
